ebae464acebe35c2d5d7a0ef21e20d3e091c7be9
Jeacy <jay.espinoza@yale.edu>
2021-04-19 07:51:36 -0700

    now the macros to change can be found in sys_changes.txt, which are automatically read and used to modify syscall.h.in using syscalln_modify.py

--- arch/arm/bits/syscalln_modify.py
+++ arch/arm/bits/syscalln_modify.py
@@ -4,6 +4,7 @@
 # "Macros"
 UNUSED = "(-1)"
 HLIMIT = 395
+FILE = "syscall.h.in"
 
 """
 @param lines a list of each line in syscall.h.in in string form
@@ -32,7 +33,7 @@ def initialize(lines, m_dict):
 
 def modify_syscall(lines_to_change = {}):
 	# open and read syscall.h.in
-	fr = open("syscall.h.in", mode = "r")
+	fr = open(FILE, mode = "r")
 	lines = fr.readlines()
 	fr.close()
 
@@ -40,13 +41,28 @@ def modify_syscall(lines_to_change = {}):
 	m_lines = initialize(lines, lines_to_change)
 
 	# open and write syscall.h.in
-	fw = open("syscall.h.in", mode = "w")
+	fw = open(FILE, mode = "w")
 	fw.writelines(m_lines)
 	fw.close()
 
+def create_dict():
+	# open and read sys_changes.txt
+	fr = open("sys_changes.txt", mode = "r")
+	lines = fr.readlines()
+	fr.close()
+	
+	output_dict = {}
+	for line in lines:
+		line = line.split(",")
+
+		if len(line) == 2:
+			output_dict[int(line[0])] = int(line[1])
+
+	return output_dict	
+
 def main():
 	# no need to worry about indexing. just enter lines as seen in file
-	lines_to_change = {}
+	lines_to_change = create_dict()
 	modify_syscall(lines_to_change) 
 
 if __name__ == "__main__":
