f1ecb2597bc804f36ac87a3e9a9f9d68def36ac0
Jeacy <jay.espinoza@yale.edu>
2021-04-19 06:01:40 -0700

    modified syscall register assignment.
    the system call number is now in r0 and following arguments are placed in r1, r2, ...

--- arch/arm/syscall_arch.h
+++ arch/arm/syscall_arch.h
@@ -36,47 +36,78 @@
 
 static inline long __syscall0(long n)
 {
+	/*
 	register long r7 __ASM____R7__ = n;
 	register long r0 __asm__("r0");
 	__asm_syscall(R7_OPERAND);
+	*/
+	register long r0 __asm__("r0") = n;
+	__asm_syscall(R0_OPERAND);
 }
 
 static inline long __syscall1(long n, long a)
-{
+{	
+	/*
 	register long r7 __ASM____R7__ = n;
 	register long r0 __asm__("r0") = a;
 	__asm_syscall(R7_OPERAND, "0"(r0));
+	*/
+	register long r0 __asm__("r0") = n;
+	register long r1 __asm__("r1") = a;
+	__asm_syscall(R0_OPERAND, "r"(r1);
 }
 
 static inline long __syscall2(long n, long a, long b)
-{
+{	
+	/*
 	register long r7 __ASM____R7__ = n;
 	register long r0 __asm__("r0") = a;
 	register long r1 __asm__("r1") = b;
 	__asm_syscall(R7_OPERAND, "0"(r0), "r"(r1));
+	*/
+	register long r0 __asm__("r0") = n;
+	register long r1 __asm__("r1") = a;
+	register long r2 __asm__("r2") = b;
+	__asm_syscall(R0_OPERAND, "r"(r1), "r"(r2));
 }
 
 static inline long __syscall3(long n, long a, long b, long c)
-{
+{	
+	/*
 	register long r7 __ASM____R7__ = n;
 	register long r0 __asm__("r0") = a;
 	register long r1 __asm__("r1") = b;
 	register long r2 __asm__("r2") = c;
 	__asm_syscall(R7_OPERAND, "0"(r0), "r"(r1), "r"(r2));
+	*/
+	register long r0 __asm__("r0") = n;
+	register long r1 __asm__("r1") = a;
+	register long r2 __asm__("r2") = b;
+	register long r3 __asm__("r3") = c;
+	__asm_syscall(R0_OPERAND, "r"(r1), "r"(r2), "r"(r3));
 }
 
 static inline long __syscall4(long n, long a, long b, long c, long d)
 {
+	/*
 	register long r7 __ASM____R7__ = n;
 	register long r0 __asm__("r0") = a;
 	register long r1 __asm__("r1") = b;
 	register long r2 __asm__("r2") = c;
 	register long r3 __asm__("r3") = d;
 	__asm_syscall(R7_OPERAND, "0"(r0), "r"(r1), "r"(r2), "r"(r3));
+	*/
+	register long r0 __asm__("r0") = n;
+	register long r1 __asm__("r1") = a;
+	register long r2 __asm__("r2") = b;
+	register long r3 __asm__("r3") = c;
+	register long r4 __asm__("r4") = d;
+	__asm_syscall(R0_OPERAND, "r"(r1), "r"(r2), "r"(r3), "r"(r4));	
 }
 
 static inline long __syscall5(long n, long a, long b, long c, long d, long e)
 {
+	/*
 	register long r7 __ASM____R7__ = n;
 	register long r0 __asm__("r0") = a;
 	register long r1 __asm__("r1") = b;
@@ -84,10 +115,19 @@ static inline long __syscall5(long n, long a, long b, long c, long d, long e)
 	register long r3 __asm__("r3") = d;
 	register long r4 __asm__("r4") = e;
 	__asm_syscall(R7_OPERAND, "0"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r4));
+	*/
+	register long r0 __asm__("r0") = n;
+	register long r1 __asm__("r1") = a;
+	register long r2 __asm__("r2") = b;
+	register long r3 __asm__("r3") = c;
+	register long r4 __asm__("r4") = d;
+	register long r5 __asm__("r5") = e;
+	__asm_syscall(R0_OPERAND, "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5));	
 }
 
 static inline long __syscall6(long n, long a, long b, long c, long d, long e, long f)
 {
+	/*
 	register long r7 __ASM____R7__ = n;
 	register long r0 __asm__("r0") = a;
 	register long r1 __asm__("r1") = b;
@@ -96,6 +136,15 @@ static inline long __syscall6(long n, long a, long b, long c, long d, long e, lo
 	register long r4 __asm__("r4") = e;
 	register long r5 __asm__("r5") = f;
 	__asm_syscall(R7_OPERAND, "0"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5));
+	*/
+	register long r0 __asm__("r0") = n;
+	register long r1 __asm__("r1") = a;
+	register long r2 __asm__("r2") = b;
+	register long r3 __asm__("r3") = c;
+	register long r4 __asm__("r4") = d;
+	register long r5 __asm__("r5") = e;
+	register long r6 __asm__("r6") = f;
+	__asm_syscall(R0_OPERAND, "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6));	
 }
 
 #define SYSCALL_FADVISE_6_ARG
